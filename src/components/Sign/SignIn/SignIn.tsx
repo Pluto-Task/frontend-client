7368aa0245a4ee53223df8fe1597463770cf523a
Andervafla <andervafla@gmail.com>
2023-05-28 04:01:46 +0300

    asd

--- src/components/Sign/SignIn/SignIn.tsx
+++ src/components/Sign/SignIn/SignIn.tsx
@@ -1,13 +1,13 @@
-import Information from "../../Information/Information";
+
 import SignInForm from "./SignInForm";
 
 const SignIn = () => {
   return (
     <>
-    <Information/>
-      {/* <div className="max-w-[428px] w-full mx-auto py-[30px]">
+    
+      <div className="max-w-[428px] w-full mx-auto py-[30px]">
         <SignInForm />
-      </div> */}
+      </div> 
     </>
   );
 };
